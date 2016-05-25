8b8e6e0f16bca9df373c35cd3e889495f2df3ee9
rhanys <rhanyskate@hotmail.com>
2016-05-25 00:56:55 -0300

    First Commition

---
+++ Angela_ParadigmaConcorrente/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Angela_ParadigmaConcorrente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonSaveGraph = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTempo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(163, 2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(104, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "START!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tempo de execução:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "...";
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(44, 27);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(872, 256);
+            this.zedGraph.TabIndex = 3;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(826, 319);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(113, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "STOP!";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result = ";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(364, 8);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(19, 13);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.Text = "....";
+            // 
+            // buttonSaveGraph
+            // 
+            this.buttonSaveGraph.Enabled = false;
+            this.buttonSaveGraph.Location = new System.Drawing.Point(315, 330);
+            this.buttonSaveGraph.Name = "buttonSaveGraph";
+            this.buttonSaveGraph.Size = new System.Drawing.Size(286, 23);
+            this.buttonSaveGraph.TabIndex = 6;
+            this.buttonSaveGraph.Text = "Save Graph IMG";
+            this.buttonSaveGraph.UseVisualStyleBackColor = true;
+            this.buttonSaveGraph.Click += new System.EventHandler(this.buttonSaveGraph_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "TEMPO:";
+            // 
+            // textBoxTempo
+            // 
+            this.textBoxTempo.Location = new System.Drawing.Point(53, 4);
+            this.textBoxTempo.Name = "textBoxTempo";
+            this.textBoxTempo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTempo.TabIndex = 7;
+            this.textBoxTempo.TextChanged += new System.EventHandler(this.textBoxTempo_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 365);
+            this.Controls.Add(this.textBoxTempo);
+            this.Controls.Add(this.buttonSaveGraph);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.zedGraph);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonSaveGraph;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTempo;
+    }
+}
+
---
+++ Angela_ParadigmaConcorrente/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ZedGraph;
+using System.Drawing.Imaging;
+
+namespace Angela_ParadigmaConcorrente
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        static int XSize = 200; //tamanho do da dimensao X do grafico
+        static int YSize = 1; //tamanho do da dimensao Y do grafico
+        Thread TWrite, TRead;
+         double[] CircularBuffer = new double[XSize];
+        Mutex MutexCircular, MutexAcesso;
+         int p1, p2;
+         
+        RollingPointPairList ListaGrafico = new RollingPointPairList(XSize);
+        int tempoX;
+        int tempoExecução, contTempo; //conta tempo de execução do programa
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double Sen_Funcao(double x)
+        {
+            double result = 0;
+
+            result = (System.Math.Sin(x) * System.Math.Sin(2 * x));
+
+            return result;
+        }
+
+         void Func1()
+        {
+            double x = 0;
+            double point = 0;
+            while (true)
+            {
+                MutexCircular.WaitOne();
+
+                if (p1 != p2)
+                {
+                    point = Sen_Funcao(x);
+                    CircularBuffer[p1] = point;
+                    p1++;
+                    x += .1;
+
+                    if ( p1 == XSize)
+                    {
+                        p1 = 0;
+                    }
+                                          
+                }
+                MutexCircular.ReleaseMutex();
+                Thread.Sleep(200);
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tempoExecução > 0)
+            {
+
+                MutexAcesso.WaitOne();
+                if (tempoX == XSize)
+                {
+                    tempoX = 0;
+                    ListaGrafico.Clear();
+                }
+                zedGraph.GraphPane.AddCurve("", ListaGrafico, Color.Blue, SymbolType.None);
+                zedGraph.Invalidate();
+                contTempo++;
+
+
+                if (contTempo == 5)
+                {
+                    tempoExecução--;
+                    label2.Text = Convert.ToString(tempoExecução) + " Segundos";
+                    contTempo = 0;
+                }
+                MutexAcesso.ReleaseMutex();
+            }
+
+            
+            else
+            {
+                    TRead.Abort();
+                    TWrite.Abort();
+                    MutexCircular.Close();
+                    MutexAcesso.Close();
+                    buttonStop.Enabled = false;
+                    buttonSaveGraph.Enabled = true;
+                    timer1.Stop();
+            } 
+
+
+
+        }
+
+         void Func2()
+        {
+            double Leitura;
+            
+            while (true)
+            {
+                MutexCircular.WaitOne();
+                    p2++;
+                    Leitura = CircularBuffer[p2];
+                if (p2 == XSize -1)
+                {
+                    p2 = -1;
+                }
+                MutexAcesso.WaitOne();
+                    ListaGrafico.Add(tempoX, Leitura);
+                    tempoX++;
+                    MutexAcesso.ReleaseMutex();
+                MutexCircular.ReleaseMutex();
+                Thread.Sleep(300);
+
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            TRead.Abort();
+            TWrite.Abort();
+            timer1.Stop();
+            MutexCircular.Close();
+            MutexAcesso.Close();
+            buttonStop.Enabled = false;
+            buttonSaveGraph.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBoxTempo_TextChanged(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = true;
+        }
+
+        private void buttonSaveGraph_Click(object sender, EventArgs e)
+        {
+            Bitmap printscreen = new Bitmap(this.Bounds.Width, this.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen);
+
+            graphics.CopyFromScreen(this.Bounds.X, this.Bounds.Y, 0, 0, this.Bounds.Size);
+            SaveFileDialog saveImageDialog = new SaveFileDialog();
+
+            saveImageDialog.Title = "Seleccionar caminho para salvar:";
+            saveImageDialog.Filter = "JPEG |*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|PNG Image|*.png|All files (*.*)|*.*";
+
+            if (saveImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                printscreen.Save(saveImageDialog.FileName, ImageFormat.Jpeg);
+                buttonSaveGraph.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            FormLoad();
+            TWrite = new Thread(Func1);
+            TRead = new Thread(Func2);
+            MutexCircular = new Mutex();
+            MutexAcesso = new Mutex();
+            tempoX = 0;
+            p1 = 0;
+            p2 = -1;
+            tempoExecução = Convert.ToInt32(textBoxTempo.Text);
+            contTempo = 0;
+
+            TWrite.Priority = ThreadPriority.AboveNormal;
+            TRead.Priority = ThreadPriority.BelowNormal;
+
+            ListaGrafico.Clear();
+            zedGraph.GraphPane.AddCurve("", ListaGrafico, Color.Blue, SymbolType.None);
+            buttonSaveGraph.Enabled = false;
+
+            TWrite.Start();
+            Thread.Sleep(500);
+            TRead.Start();
+            timer1.Start();
+
+
+        }
+
+        private void FormLoad()
+        {
+            zedGraph.GraphPane.Title.Text = "Sinal Gerado";
+            zedGraph.GraphPane.XAxis.Title.Text = "Tempo";
+            zedGraph.GraphPane.YAxis.Title.Text = "Amplitude";
+            zedGraph.GraphPane.XAxis.Scale.Min = 0;
+            zedGraph.GraphPane.XAxis.Scale.Max = XSize;
+            zedGraph.GraphPane.XAxis.Scale.MinorStep = 1;
+            zedGraph.GraphPane.XAxis.Scale.MajorStep = 5;
+            zedGraph.GraphPane.YAxis.Scale.Min = -1;
+            zedGraph.GraphPane.YAxis.Scale.Max = YSize;
+            buttonStop.Enabled = true;
+
+        }
+    }
+}
